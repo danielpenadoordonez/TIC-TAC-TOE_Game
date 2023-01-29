b97cf1d591c944d7821afb58d2172f2706eaadac
Daniel Penado <penadodan02@gmail.com>
2023-01-28 22:55:36 -0600

    Score section, general design and structure improved

--- WEB_Game/frontend/tic-tac-toe/src/components/guide/Score.js
+++ WEB_Game/frontend/tic-tac-toe/src/components/guide/Score.js
@@ -4,18 +4,35 @@ import './Score.css';
 const Score = (props) => {
     const score = props.score;
 
+    // return (
+    //     <div className="guide-card">
+    //         <div className="symbol-area" id="computer-symbol">
+    //             <h3>{score.machine}</h3>
+    //         </div>
+    //        <h3>Computer</h3>
+    //        <br></br>
+    //        <br></br>
+    //        <div className="symbol-area" id="player-symbol">
+    //             <h3>{score.player}</h3>
+    //         </div>
+    //         <h3>Player</h3>
+    //     </div>
+    // );
+
     return (
         <div className="guide-card">
-            <div className="symbol-area" id="computer-symbol">
-                <h3>{score.machine}</h3>
-            </div>
-           <h3>Computer</h3>
-           <br></br>
-           <br></br>
-           <div className="symbol-area" id="player-symbol">
-                <h3>{score.player}</h3>
-            </div>
-            <h3>Player</h3>
+            <section className="score" id="player-section">
+                <h3>Player</h3>
+                <div className="symbol-area" id="player-symbol">
+                    <h3>{score.player}</h3>
+                </div>
+            </section>
+            <section className="score" id="computer-section">
+                <h3>Computer</h3>
+                <div className="symbol-area" id="computer-symbol">
+                    <h3>{score.machine}</h3>
+                </div>
+            </section>
         </div>
     );
 }
